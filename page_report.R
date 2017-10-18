c17a565e73ff449ff1b54465b0bb74b6bca6213c
sgorka <30950966+sgorka@users.noreply.github.com>
2017-10-18 23:18:31 +0200

    Add select country

--- page_report.R
+++ page_report.R
@@ -20,10 +20,11 @@ pisa <- PISA2012lite::student2012 %>%
     ST94Q05:ST94Q14, 
     ST96Q01:ST96Q05, 
     ST101Q01:ST101Q05, 
-    ST104Q01:ST104Q06
+    ST104Q01:ST104Q06,
+    CNT
   ) %>%
-  rename(gender = ST04Q01)
-
+  rename(gender = ST04Q01) %>% 
+  filter(CNT=="Poland")
 pisa[] <- lapply(pisa, factor)
 # pick only 40th question
 only_40 <- pisa %>% select(
@@ -43,8 +44,11 @@ only_40_1 <- only_40 %>%
 only_40_m <- only_40_1 %>% 
   filter(gender=="Male") 
 ggplot(only_40_m, aes(x=question,y= freq,fill=level)) + geom_bar(stat = "identity")
+only_40_f <- only_40_1 %>% 
+  filter(gender=="Female") 
+ggplot(only_40_f, aes(x=question,y= freq,fill=level)) + geom_bar(stat = "identity")
   
-
+# kolory - 1,2 odcienie zielony; 3,4 - odcienie czerwonego
 
 
 
