b8d427b46e80b53a44608f5e952ea7b14f6ef05c
sgorka <30950966+sgorka@users.noreply.github.com>
2017-10-19 19:51:15 +0200

    Fix graphs

--- page_report.R
+++ page_report.R
@@ -3,6 +3,39 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
+library(gridExtra)
+library(scales)
+
+# fnkcja z https://github.com/tidyverse/ggplot2/wiki/share-a-legend-between-two-ggplot2-graphs
+grid_arrange_shared_legend <- function(..., ncol = length(list(...)), nrow = 1, position = c("bottom", "right")) {
+  
+  plots <- list(...)
+  position <- match.arg(position)
+  g <- ggplotGrob(plots[[1]] + theme(legend.position = position))$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  lwidth <- sum(legend$width)
+  gl <- lapply(plots, function(x) x + theme(legend.position="none"))
+  gl <- c(gl, ncol = ncol, nrow = nrow)
+  
+  combined <- switch(position,
+                     "bottom" = arrangeGrob(do.call(arrangeGrob, gl),
+                                            legend,
+                                            ncol = 1,
+                                            heights = unit.c(unit(1, "npc") - lheight, lheight)),
+                     "right" = arrangeGrob(do.call(arrangeGrob, gl),
+                                           legend,
+                                           ncol = 2,
+                                           widths = unit.c(unit(1, "npc") - lwidth, lwidth)))
+  
+  grid.newpage()
+  grid.draw(combined)
+  
+  # return gtable invisibly
+  invisible(combined)
+  
+}
+
 
 get_data <- function(questions = c()) {
   PISA2012lite::student2012 %>% 
@@ -57,4 +90,70 @@ grid.arrange(plot_m, plot_f, nrow=1, ncol=2)
 
 
 #===============================
+only_38 <- pisa %>% select(
+  gender,
+  ST96Q01:ST96Q05
+)
+
+
+only_38_1 <- only_38 %>% 
+  filter(!is.na(ST96Q01),!is.na(ST96Q02), !is.na(ST96Q03), !is.na(ST96Q05)) %>% 
+  gather("question", "level", ST96Q01:ST96Q05) %>% 
+  group_by(gender, question, level ) %>% 
+  summarise (n = n()) %>% 
+  mutate(freq = n / sum(n)) %>% 
+  select(-n) 
+
+only_38_m <- only_38_1 %>% 
+  filter(gender=="Male") 
+plot_m <- ggplot(only_38_m, aes(x=question,y= freq,fill=level)) + geom_bar(stat = "identity") + 
+  scale_fill_manual(values=c("olivedrab", "olivedrab3", "firebrick2","red4")) + ggtitle("Male") + 
+  geom_text(aes(label=paste0(sprintf("%.0f", freq*100),"%")),position=position_stack(vjust=0.5))   +
+  scale_y_continuous(labels = percent_format())
+
+only_38_f <- only_38_1 %>% 
+  filter(gender=="Female") 
+plot_f <- ggplot(only_38_f, aes(x=question,y= freq,fill=level)) + geom_bar(stat = "identity") + 
+  scale_fill_manual(values=c("olivedrab", "olivedrab3", "firebrick2","red4")) + ggtitle("Female") +
+  geom_text(aes(label=paste0(sprintf("%.0f", freq*100),"%")),position=position_stack(vjust=0.5)) + 
+  scale_y_continuous(labels = percent_format())
+
+grid.arrange(plot_m, plot_f, nrow=1, ncol=2)
+grid_arrange_shared_legend(plot_m, plot_f)
+
+q38a <- only_38_1 %>% 
+  filter(question=="ST96Q01")
+plot_a <- ggplot(q38a, aes(x=gender,y= freq,fill=level)) + geom_bar(stat = "identity") + 
+  scale_fill_manual(values=c("olivedrab", "olivedrab3", "firebrick2","red4")) + ggtitle("Female") +
+  geom_text(aes(label=paste0(sprintf("%.0f", freq*100),"%")),position=position_stack(vjust=0.5)) + 
+  scale_y_continuous(labels = percent_format())
+
+
+  q <- unique(only_38_1$question)
+  
+
+
+    
+  plot_bar <- function(data) {  
+    plot <- ggplot(data, aes(x=gender,y= freq,fill=level)) + geom_bar(stat = "identity") + 
+      scale_fill_manual(values=c("olivedrab", "olivedrab3", "firebrick2","red4")) + ggtitle("Female") +
+      geom_text(aes(label=paste0(sprintf("%.0f", freq*100),"%")),position=position_stack(vjust=0.5)) + 
+      scale_y_continuous(labels = percent_format())
+    return(plot)
+  }
+  
+a <- only_38_1 %>% 
+  filter(question=="ST96Q01")
+b <- only_38_1 %>% 
+  filter(question=="ST96Q02")
+c <- only_38_1 %>% 
+  filter(question=="ST96Q03")
+d <- a <- only_38_1 %>% 
+  filter(question=="ST96Q05") 
+ plot_bar(a)
+ plot_bar(b)
+ plot_bar(c)
+ plot_bar(d)
+grid_arrange_shared_legend(plot_bar(a), plot_bar(b), plot_bar(c), plot_bar(d))
+
 
